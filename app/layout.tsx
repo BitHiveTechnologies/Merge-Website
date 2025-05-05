7bd31887a240bc6be2c949bc62fdf9dccf5d577b
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2025-05-05 17:45:51 +0530

    Enabled Speed Insights and Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
+
 export const metadata: Metadata = {
     title: 'Merge',
     icons: {
@@ -11,6 +14,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <html lang="en" data-oid="4os:rc.">
             <body className="" data-oid="z3n9x7y">
                 {children}
+                <SpeedInsights data-oid="tzn2sfn" />
+                <Analytics data-oid="yvy367l" />
             </body>
         </html>
     );
